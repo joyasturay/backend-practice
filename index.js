60bcee95c58d06aa6edc6a4f0528927bc9b62abd
joyasturay <joyasturay1@gmail.com>
2024-01-08 22:39:30 +0530

    random words

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let a=8;
+/*let a=8;
 console.log(a);
 import {sum,mul} from "./math.js";
 console.log(sum(2,3));
-console.log(mul(2,3));
+console.log(mul(2,3));*/
+import { generate} from "random-words";
+
+console.log(generate());
